dda1d2b560e70abca794cde37851442530f3baa1
Benjamin Alijagic <ben@wivern.co>
2014-05-18 15:06:10 +0000

    Frontend for /answers v1

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready page:load', function () {
+
+	// $(".dropdown,.dropdown-menu").hover(function(){
+	// 	$(".dropdown-menu").show();
+	// }, function(){
+	// 	$(".dropdown-menu").hide();
+	// });
+
+});
